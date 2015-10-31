94237bcef508965885969f68fb0dbfb93de6f925
aFarkas <info@corrupt-system.de>
2015-10-31 12:24:24 +0100

    ...

--- sources/js/libs/rb_color-hook.js
+++ sources/js/libs/rb_color-hook.js
@@ -16,13 +16,13 @@
 
 		color = color.replace(regWhite, '');
 
-		if (matchedColor = regSixHex.exec(color)){
+		if ((matchedColor = regSixHex.exec(color))){
 			parsedColor = [parseInt(matchedColor[1], 16), parseInt(matchedColor[2], 16), parseInt(matchedColor[3], 16)];
-		} else if (matchedColor = regThreeHex.exec(color)){
+		} else if ((matchedColor = regThreeHex.exec(color))){
 			parsedColor = [parseInt(matchedColor[1], 16) * 17, parseInt(matchedColor[2], 16) * 17, parseInt(matchedColor[3], 16) * 17];
-		} else if (matchedColor = regRgba.exec(color)){
+		} else if ((matchedColor = regRgba.exec(color))){
 			parsedColor = [+matchedColor[1], +matchedColor[2], +matchedColor[3], +matchedColor[4]];
-		} else if (matchedColor = regRgb.exec(color)){
+		} else if ((matchedColor = regRgb.exec(color))){
 			parsedColor = [+matchedColor[1], +matchedColor[2], +matchedColor[3]];
 		}
 
@@ -34,25 +34,6 @@
 		return parsedColor;
 	};
 
-	$.fx.step.backgroundColor = function( fx ) {
-		var i, tmpValue;
-		if(!fx.parsedColors){
-			fx.parsedColors = {
-				start: parseColor(fx.start),
-				end: parseColor(fx.end),
-			}
-		}
-		fx.now = [];
-		for(i = 0; i < fx.parsedColors.start.length; i++){
-			tmpValue = (fx.parsedColors.end[i] - fx.parsedColors.start[i]) * fx.pos + fx.parsedColors.start[i];
-			if(i != 3){
-				tmpValue = Math.round(tmpValue);
-			}
-			fx.now.push(tmpValue);
-		}
-		fx.elem.style.backgroundColor = 'rgba('+ fx.now.join(',') +')';
-	};
-
 	['backgroundColor', 'color', 'borderTopColor', 'borderRightColor', 'borderBottomColor', 'borderLeftColor', 'columnRuleColor', 'outlineColor', 'textDecorationColor', 'textEmphasisColor'].forEach(function(name){
 		if(!$.fx.step[name]){
 
@@ -62,7 +43,7 @@
 					fx.parsedColors = {
 						start: parseColor(fx.start),
 						end: parseColor(fx.end),
-					}
+					};
 				}
 				fx.now = [];
 				for(i = 0; i < fx.parsedColors.start.length; i++){
