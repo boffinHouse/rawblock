2b991860cb964f79b077cc1eba00230aeceaa787
alexander.farkas <info@corrupt-system.de>
2016-05-25 16:33:04 +0200

    0.2.23

---
+++ sources/js/utils/rb_debounce.js
@@ -0,0 +1,47 @@
+(function (factory) {
+    if (typeof module === 'object' && module.exports) {
+        module.exports = factory();
+    } else {
+        factory();
+    }
+}(function () {
+    'use strict';
+    /* jshint eqnull: true */
+    var rb = window.rb || {};
+
+	/**
+     *
+     * @param fn
+     * @param opts
+     * @param opts.delay
+     * @param opts.that
+     * @returns {Function}
+     */
+    rb.debounce = function(fn, opts){
+        var args, that, timestamp, timeout;
+
+        var later = function(){
+            var last = Date.now() - timestamp;
+
+            if (last < opts.delay) {
+                setTimeout(later, opts.delay - last);
+            } else {
+                fn.apply(that, args);
+            }
+        };
+
+        opts = Object.assign({delay: 100}, opts);
+
+        return function(){
+            timestamp = Date.now();
+            args = arguments;
+            that = opts.that || this;
+
+            if (!timeout) {
+                timeout = setTimeout(later, opts.delay);
+            }
+        };
+    };
+
+    return rb.debounce;
+}));
